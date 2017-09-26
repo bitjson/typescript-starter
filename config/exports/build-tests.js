261f3941f6556966bfcd68710c1111ecea0eb039
Mitchell Barry <mitch.barry@gmail.com>
2017-09-26 13:03:56 -0500

    fix(tests): use proper OS separator when loading test modules
    Fixes bitjson/typescript-starter#18

--- config/exports/build-tests.js
+++ config/exports/build-tests.js
@@ -1,23 +1,30 @@
 // this script watches the tests exported by typescript, copies them to the test directories, and modifies the require("PKG.NAME") statements to test each build
 const cpx = require("cpx");
-const separator = require("path").sep;
+const isWin = /^win/.test(process.platform);
 const Transform = require("stream").Transform;
 const pkg = require('../../package');
-const req = (path) => 'require("' + path + '")';
-const pathUp = (levels) => Array.from(Array(levels), () => '../').join('');
+const req = (modulePath) => 'require("' + modulePath + '")';
+const calculatedSeparator = isWin ? '\\\\' : '/';
+const pathUp = (levels) => Array.from(Array(levels), () => '..' + calculatedSeparator).join('');
+const normalizePath = (filePath) => filePath.replace(/\//g, calculatedSeparator);
 
 // replace instances of pkg.name with the proper route to the build being tested
 const makeTransform = (filePath, buildPath) => {
-  const buildPathParts = buildPath.split(separator);
+  if (isWin) {
+    filePath = normalizePath(filePath);
+    buildPath = normalizePath(buildPath);
+  }
+
+  const buildPathParts = buildPath.split(calculatedSeparator);
   // filePath includes build/main (-2), test/BUILD is 2 deep (+2),
   // remove filename (-1). Total is length - 2
-  const pathToRoot = pathUp(filePath.split(separator).length - 1);
+  const pathToRoot = pathUp(filePath.split(calculatedSeparator).length - 1);
   const placeholder = req(pkg.name);
   return new Transform({
     transform(chunk, encoding, done) {
       const str = chunk.toString();
-      const parts = str.split(placeholder)
-      const newPath = req(pathToRoot + buildPath)
+      const parts = str.split(placeholder);
+      const newPath = req(pathToRoot + buildPath);
       const result = parts.join(newPath);
       this.push(result);
       done();
